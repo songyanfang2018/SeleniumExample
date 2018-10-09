8a491318b14b8aacadc7d9a566b73577b1931d88
Yanfang Song <songyanfang.cn@gmail.com>
2018-10-09 13:26:08 +0200

    filter nach versandkostenfrei

---
+++ src/test/java/song/selenium/catchtestwork/CategoryTest.java
@@ -0,0 +1,70 @@
+package song.selenium.catchtestwork;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(Parameterized.class)
+
+public class CategoryTest extends AbstractSeleniumTest{
+    private int index;
+
+
+    public CategoryTest(int index) {
+        this.index = index;
+    }
+
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        List<Object[]> result = new ArrayList<>();
+
+        for (int i = 0; i < 30; i++) {
+            result.add(new Object[]{i});
+        }
+
+        return result;
+    }
+
+
+    @Test
+
+    public void CategoryTest(){
+        System.out.println("Testing index: " + index);
+
+    final List<WebElement> categoryList = driver.findElements(By.cssSelector(".nav-item"));
+        if (index >= categoryList.size()) {
+        return;
+    }
+
+        final WebElement category = categoryList.get(index);
+        category.click();
+
+        final List<WebElement> categoryList2 = driver.findElements(By.cssSelector(".nav-link"));
+        if (index >= categoryList2.size()) {
+            return;
+        }
+
+        final List<WebElement> cards = driver.findElements(By.cssSelector(".card"));
+        wait.until(ExpectedConditions.visibilityOfAllElements(cards));
+
+        if (cards.size() <= 0) {
+            System.out.println("ERROR: Category " + category.findElement(By.cssSelector(".category-name")).getText()
+                    + " has no item");
+        }
+        assertThat(cards.size()).isGreaterThan(0);
+
+
+    }
+
+}
